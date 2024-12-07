19e4647fcdb5213095b0f19e4cff65e7268c5c9d
sonjongwook123 <sonjongwook123@gmail.com>
2024-12-07 17:41:04 +0900

    아답터 패턴 업뎃

---
+++ Assets/Script/10.Adapter/InventorySystemAdapter.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter.Adapter
+{
+    public class InventorySystemAdapter : InventorySystem, IInventorySystem
+    {
+        private List<InventoryItem> _cloudInventory;
+
+        public void SyncInventories()
+        {
+            var _cloudInventory = GetInventory();
+            
+            Debug.Log("Synchronizing local drive and cloud inventories");
+        }
+        
+        public void AddItem(InventoryItem item, SaveLocation location)
+        {
+            if(location == SaveLocation.Cloud)
+            {
+                AddItem(item);
+            }
+            
+            if(location == SaveLocation.Local)
+            {
+                Debug.Log("Adding item to local drive");
+            }
+            
+            if(location == SaveLocation.Both)
+            {
+                Debug.Log("Adding item to local drive and on the cloud");
+            }
+        }
+        
+        public void RemoveItem(InventoryItem item, SaveLocation location)
+        {
+            Debug.Log("Remove item from local/cloud/both");
+        }
+
+        public List<InventoryItem> GetInventory(SaveLocation location)
+        {
+            Debug.Log("Remove item from local/cloud/both");
+
+            return new List<InventoryItem>();
+        }
+    }
+}
